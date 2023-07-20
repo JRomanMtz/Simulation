2cb8b744c0b677fd7380566efe1595be182226e5
JRomanMtz <140033556+JRomanMtz@users.noreply.github.com>
2023-07-19 20:38:43 -0600

    DiscreteRandcomVariable.r

---
+++ DiscreteRandcomVariable.r
@@ -0,0 +1,23 @@
+x <- c(1,2,3,4,5)
+pi <- c(.2,.3,.1,.15,.25)
+acumuladas <- cumsum(pi)
+
+n <- 1000
+for(k in 1:n){
+u <- runif(1)
+for(i in 1:length(acumuladas)){
+  if( u < acumuladas[i] ){
+    va <- x[i]
+    break()
+  }
+}
+
+if(k == 1){
+  xf <- va
+}else{
+  xf <- c(xf,va)
+}
+
+}
+hist(xf,breaks = c(0:max(xf)))
+table(xf)
